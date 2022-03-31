8fdb77ee5f350238eb75298839e3a70aa58d1ae6
Maingron <r@maingron.com>
2022-03-31 10:59:21 +0200

    Add templates to date-display module; Add function getDayOfWeekString() to date-display module

--- modules/date-display/init.js
+++ modules/date-display/init.js
@@ -1,1 +1,23 @@
-loadHTMLTemplate("modules/date-display/mytemplate.html")
+loadHTMLTemplate("modules/date-display/mytemplate.html");
+
+function getDayOfWeekString(which) {
+    switch(which) {
+        case 1:
+            return "Monday"
+        case 2:
+            return "Tuesday"
+        case 3:
+            return "Wednesday"
+        case 4:
+            return "Thursday"
+        case 5:
+            return "Friday"
+        case 6:
+            return "Saturday"
+        case 0:
+            return "Sunday"
+        default:
+            return which + " not found. Maybe it's Monday?" // Error if day not found
+    }
+
+}
